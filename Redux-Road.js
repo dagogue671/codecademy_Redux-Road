3823d0463194fcaf6084127c0aa0ca60e58fdeca
David Gogue <dagogue671@gmail.com>
2024-05-08 12:13:14 +1000

    Update Redux-Road.js

--- Redux-Road.js
+++ Redux-Road.js
@@ -1,129 +1,86 @@
-//Base State 
-const initailWagonState = {
-    supplies: 100,
-    distance: 0,
-    days: 0,
-    cash: 200,
-  };
-  
-  const gameReducer = (state = initailWagonState, action) => {
-    switch(action.type)
-    {
-      case 'gather':{
-        return {
-          ...state,
-          supplies: state.supplies + 15,
-          distance: state.distance,
-          days: state.days + 1,
-          cash: state.cash
-        }
+const initialWagonState = {
+  supplies: 100, 
+  distance: 0, 
+  days: 0,
+  cash: 200
+}
+
+const reducer = (state = initialWagonState, action) => {
+  switch(action.type){
+    default:{
+      return state;
+    }
+    case 'gather':{
+      return {
+        ...state,
+        supplies: state.supplies + 15,
+        days: state.days + 1
       }
-      case 'travel':{
-        const suppliesNeeded = 20 * action.payload;
-        if(state.supplies < suppliesNeeded){
-            console.log('Not Enough Supplies');
-            return state;
-        }
-        else {
-            return {
-                ...state,
-                supplies: state.supplies - 20 * action.payload,
-                distance: state.distance + 10 * action.payload,
-                days: state.days + action.payload,
-                cash: state.cash
-              };
-        }
-  
-      }
-      case 'tippedWagon' : {
-        return {
-          ...state,
-        supplies: state.supplies - 30,
-        distance: state.distance,
-        days: state.days + 1,
-        cash: state.cash
-        }
-      }
-      case 'sell': {
-        if(state.supplies < 20){
-          console.log('Not Enough Supplies to Sell');
-        }
-        else{
+    }
+    case 'travel': {
+      const suppliesNeeded = state.supplies - (20 * action.payload);
+      if(suppliesNeeded >= 0){
           return {
-          ...state,
-          supplies: state.supplies - 20,
-          distance: state.distance,
-          days: state.days,
-          cash: state.cash + 5
-        };
+        ...state,
+        supplies: state.supplies - (20 * action.payload),
+        distance: state.distance + (10 * action.payload),
+        days: state.days + action.payload 
         }
       }
-      case 'buy' : {
-       if(state.cash < 25){
-        console.log('Not Enough Cash');
-       }
-       else {
-        return {
-          ...state,
-          supplies: state.supplies + 25,
-          distance: state.distance,
-          days: state.days,
-          cash: state.cash - 15
-        }
-       }
-      }
-       case 'theft' : {
-        if(state.cash !== 0){
-            console.log('OHH NO!!! I GOT ROBBED!!!');
-            return {
-             ...state,
-            supplies: state.supplies,
-            distance: state.distance,
-            days: state.days,
-            cash: Math.ceil(state.cash/2)
-            }
-        }
-       else{
-        console.log('What a loser!! I don\'t even have any money.');
+      else{
+        console.log('Not enough Supplies for travel.');
         return state;
-       }
-    
       }
-      default:{
+    }
+    case 'tippedWagon': {
+      return {
+        ...state,
+        supplies: state.supplies - 30,
+        days: state.days + 1
+      }
+    }
+    case 'sell': {
+      if(state.supplies >= 20){
+           return {
+        ...state,
+        supplies: state.supplies - 20,
+        cash: state.cash + 5
+        }
+      }
+      else {
+        console.log('Not enough Supplies');
+        return state;
+      }
+    }
+    case 'buy': {
+      if(state.cash > 15) {
+           return {
+        ...state,
+        supplies: state.supplies + 25,
+        cash: state.cash - 15
+        }
+      }
+      else {
+        console.log('Not enough Cash.');
         return state;
       }
     }
   }
-  
-  //Initialized State of Wagon/Base State
-  let wagon = gameReducer(undefined, {});
-  console.log(wagon);
+}
 
-  //Day 1
-  wagon = gameReducer(wagon,{type:'travel',payload: 1});
-  console.log(wagon);
-
-  //Day 2
-  wagon = gameReducer(wagon,{type:'gather'});
-  console.log(wagon);
-
-  //Day 3
-  wagon = gameReducer(wagon,{type:'tippedWagon'});
-  console.log(wagon);
-
-  //Day 4
-  wagon = gameReducer(wagon,{type:'travel', payload: 3});
-  console.log(wagon);
-
-  //Day 5 flag console that there is not enough supplies
-  wagon = gameReducer(wagon,{type:'travel', payload: 3});
-  console.log(wagon);
-
-  //Robbed that same day
-  wagon = gameReducer(wagon,{type:'theft'});
-  console.log(wagon);
-  
-
-  
-  
-  
+let wagon = reducer(undefined, {});
+console.log(wagon);
+wagon = reducer(wagon, {type: 'travel', payload: 1});
+console.log(wagon);
+wagon = reducer(wagon,{type: 'gather'});
+console.log(wagon);
+wagon = reducer(wagon, {type: 'tippedWagon'});
+console.log(wagon);
+wagon = reducer(wagon, {type: 'travel', payload: 3});
+console.log(wagon);
+wagon = reducer(wagon, {type: 'travel', payload: 3});
+console.log(wagon);
+wagon = reducer(wagon, {type: 'sell'});
+console.log(wagon);
+wagon = reducer(wagon, {type: 'buy'});
+console.log(wagon);
